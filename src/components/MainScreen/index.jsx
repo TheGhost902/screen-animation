607213d2e486153b65100984ccb05ccc97184265
TheGhost902 <emilc902@gmail.com>
2020-07-31 23:20:33 +0300

    Added touch events

--- src/components/MainScreen/index.jsx
+++ src/components/MainScreen/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 import './MainScreen.scss'
 
@@ -6,6 +6,7 @@ export const ScrollContext = React.createContext(0)
 
 export function MainScreen({ children }) {
     const [screenNumber, setScreenNumber] = useState(0)
+    const clientYRef = useRef(0)
 
     function scrollHandler(e) {
         const delta = e.nativeEvent.wheelDeltaY
@@ -17,8 +18,23 @@ export function MainScreen({ children }) {
         if (delta < 0) setScreenNumber(screenNumber + 1)
     }
 
+    function touchStartHandler(e) {
+        clientYRef.current = e.nativeEvent.touches[0].clientY
+    }
+    function touchEndHandler(e) {
+        const { clientY } = e.nativeEvent.changedTouches[0]
+
+        if (clientY > clientYRef.current) setScreenNumber(screenNumber - 1)
+        if (clientY < clientYRef.current) setScreenNumber(screenNumber + 1)
+    }
+
     return (
-        <div className="main-screen" onWheel={scrollHandler}>
+        <div
+            className="main-screen"
+            onWheel={scrollHandler}
+            onTouchStart={touchStartHandler}
+            onTouchEnd={touchEndHandler}
+        >
             <div className="main-screen__status">{screenNumber + 1}</div>
 
             <ScrollContext.Provider value={screenNumber}>
