1e35ca75d74ab46ee85b54643cd840d9c9f791f8
TheGhost902 <emilc902@gmail.com>
2020-08-02 19:35:43 +0300

    Updates

---
+++ precache-manifest.5ac2f5678547a34ca2eaac21d2a23b06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb578f7197d3920a63330836c78f5ed9",
+    "url": "/screen-animation/index.html"
+  },
+  {
+    "revision": "e383c8298115c095eee1",
+    "url": "/screen-animation/static/css/main.b5ef20fc.chunk.css"
+  },
+  {
+    "revision": "213be132647a182821d2",
+    "url": "/screen-animation/static/js/2.4219b553.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/screen-animation/static/js/2.4219b553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e383c8298115c095eee1",
+    "url": "/screen-animation/static/js/main.4198f5d7.chunk.js"
+  },
+  {
+    "revision": "e3b900132220f48b8a4f",
+    "url": "/screen-animation/static/js/runtime-main.9009f355.js"
+  },
+  {
+    "revision": "a70075399eb7c8c97025310f5ca4db67",
+    "url": "/screen-animation/static/media/bg.a7007539.jpg"
+  }
+]);
